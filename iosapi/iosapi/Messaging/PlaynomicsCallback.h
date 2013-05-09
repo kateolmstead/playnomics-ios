b7653720ee4bbf0928a377cf6fc55fc70ada65f2
Shiraz <shiraz@playnomics.com>
2013-05-09 12:08:21 -0700

    Moved constant into it's appropriate file (PNConstants.h)

--- iosapi/iosapi/Messaging/PlaynomicsCallback.h
+++ iosapi/iosapi/Messaging/PlaynomicsCallback.h
@@ -8,8 +8,6 @@
 
 #import <Foundation/Foundation.h>
 
-#define kPushCallbackUrl         @"t"//push payloads will carry this var
-
 @interface PlaynomicsCallback : NSObject
 - (void)submitAdImpressionToServer:(NSString *)impressionUrl ;
 @end
