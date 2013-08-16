fceec546202409d94cd6b999cbbdba98dc49bda7
Jared Jenkins <jared@playnomics.com>
2013-08-16 12:37:20 -0700

    Adding a new image view child class to handle lower level touch events.

--- iosapi/iosapi/Messaging/PNUIImageView.h
+++ iosapi/iosapi/Messaging/PNUIImageView.h
@@ -8,6 +8,33 @@
 
 #import <UIKit/UIKit.h>
 
+typedef struct {
+    int width;
+    int height;
+    int x;
+    int y;
+} PNViewDimensions;
+
+@protocol PNUIImageDelegate
+-(void) didLoad;
+-(void) didFailToLoad;
+-(void) didFailToLoadWithError: (NSError*) error;
+-(void) didFailToLoadWithException: (NSException*) exception;
+-(void) didReceiveTouch: (UITouch*) touch;
+@end
+
 @interface PNUIImageView : UIImageView
+
+@property PNViewDimensions dimensions;
+
+//make a copy of the String, to ensure that it's value can't change
+@property (copy, readonly) NSString* imageUrl;
+
+//assign, makes this reference weak. This because we aren't creating our delegate object, this prevents
+//strong references cycles.
+@property (assign) id<PNUIImageDelegate> delegate;
+
+-(id) initWithDimensions: (PNViewDimensions) dimensions delegate: (id<PNUIImageDelegate>) delegate;
+-(id) initWithDimensions: (PNViewDimensions) dimensions delegate: (id<PNUIImageDelegate>) delegate imageUrl:(NSString*) imageUrl;
 
 @end
--- iosapi/iosapi/Messaging/PNUIImageView.m
+++ iosapi/iosapi/Messaging/PNUIImageView.m
@@ -7,38 +7,83 @@
 //
 
 #import "PNUIImageView.h"
+#import "FSNConnection.h"
 
 @implementation PNUIImageView
 
-- (id)initWithFrame:(CGRect)frame
-{
-    self = [super initWithFrame:frame];
-    if (self) {
-        // Initialization code
+@synthesize imageUrl = _imageUrl;
+@synthesize delegate = _delegate;
+@synthesize dimensions = _dimensions;
+
+-(id) initWithDimensions: (PNViewDimensions) dimensions delegate: (id<PNUIImageDelegate>) delegate{
+    self = [super init];
+    if(self){
+        [self setUserInteractionEnabled: YES];
+        [self setExclusiveTouch: YES];
+        _delegate = delegate;
+        _dimensions = dimensions;
+        self.frame = CGRectMake(dimensions.x, dimensions.y, dimensions.width, dimensions.height);
+        //no image to load this component is ready to go
+        [self.delegate didLoad];
     }
     return self;
 }
 
+-(id) initWithWidth: (PNViewDimensions) dimensions delegate: (id<PNUIImageDelegate>) delegate imageUrl: (NSString*) imageUrl{
+    self = [super init];
+    if(self){
+        [self setUserInteractionEnabled: YES];
+        [self setExclusiveTouch: YES];
+        _delegate = delegate;
+        _dimensions = dimensions;
+        self.frame = CGRectMake(dimensions.x, dimensions.y, dimensions.width, dimensions.height);
+        if(imageUrl != nil && imageUrl != (id)[NSNull null] ){
+            _imageUrl = imageUrl;
+            [self loadImage];
+        } else{
+            //no image to load this component is ready to go
+            [self.delegate didLoad];
+        }
+    }
+    return self;
+}
 
+-(void)loadImage{
+    //load the image here
+    NSURL* url = [NSURL URLWithString:self.imageUrl];
+    
+    if (url == nil){
+        [self.delegate didFailToLoadWithException:nil];
+        return;
+    }
+    
+    FSNConnection* connection =[FSNConnection withUrl:url
+                                               method:FSNRequestMethodGET
+                                              headers:nil
+                                           parameters:nil
+                                           parseBlock:nil
+                                      completionBlock:^(FSNConnection *c) { [self onImageDownloaded:c]; }
+                                        progressBlock:nil];
+    
+    [connection start];
+}
+
+- (void)onImageDownloaded:(FSNConnection *)connection {
+    if (connection.error) {
+        //NSLog(@"Error retrieving image from the internet: %@", connection.error.localizedDescription);
+        [self.delegate didFailToLoadWithError: connection.error];
+    } else {
+        self.image = [UIImage imageWithData:connection.responseData];
+        [self.delegate didLoad];
+    }
+}
+
+#pragma mark "Touch Events"
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
-    
+    NSEnumerator *enumerator = [touches objectEnumerator];
+    id value;
+    if ((value = [enumerator nextObject]) && [value isKindOfClass:[UITouch class]]) {
+        [self.delegate didReceiveTouch: (UITouch*)value];
+    }
 }
-
--(void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event{
-    
-}
-
--(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event{
-    
-}
-
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
-
 @end
