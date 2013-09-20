9c08c595dc73a5e812ae0038a8ad09f43ffb214e
Jared Jenkins <jared@playnomics.com>
2013-09-19 18:25:27 -0700

    Updating Cocos2dx sample app

--- cocos2dxSample/Cocoas2dxSample/ios/MarshalLayer.mm
+++ cocos2dxSample/Cocoas2dxSample/ios/MarshalLayer.mm
@@ -5,15 +5,11 @@
 //  Created by Jared Jenkins on 8/15/13.
 //
 //
-
-#import "PlaynomicsMessaging.h"
-#import "PlaynomicsFrame.h"
+#import "Playnomics.h"
 #import "MarshalLayer.h"
 
 void pn_loadFrame(const char* frameId){
     NSString* utfFrameId = [[NSString alloc] initWithUTF8String:frameId];
-    PlaynomicsMessaging* messaging = [PlaynomicsMessaging sharedInstance];
-    PlaynomicsFrame* frame = [messaging createFrameWithId: utfFrameId];
-    [frame start];
+    [Playnomics showFrameWithId:utfFrameId];
     [utfFrameId release];
 }
